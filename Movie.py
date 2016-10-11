e468f3e636075f44aed5e0e1669786a82fa6505f
Adele Kapellusch <akoutia@email.arizona.edu>
2016-10-11 07:21:10 -0700

    Updated Movie.py
    Updated to delete.png and.txt files

--- Movie.py
+++ Movie.py
@@ -4,3 +4,6 @@ import sys
 commandlineArgs = sys.argv[1:]
 
 os.system("/home/u14/akoutia/homework/hw10/ffmpeg/3.1.3/bin/ffmpeg -r " + commandlineArgs[0] + " -i %03d.png -r ntsc " + commandlineArgs[1] +"-y")
+
+[os.remove(path + f) for f in os.listdir(path) if f.endswith(".png")]
+[os.remove("../" + f) for f in os.listdir("../") if f.endswith(".txt")]
